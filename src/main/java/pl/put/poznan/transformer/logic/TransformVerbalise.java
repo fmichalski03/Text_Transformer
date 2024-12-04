d438c9fd3d1d45eefedd2e89ad8a62fc73813b00
Krzysztof Smal <krz.smal@gmail.com>
2024-12-04 12:30:59 +0100

    Dodano dokumnentację do klasy TransformVerbalise

--- src/main/java/pl/put/poznan/transformer/logic/TransformVerbalise.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformVerbalise.java
@@ -5,18 +5,64 @@ import org.slf4j.LoggerFactory;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+/**
+ * A class that decorates text by transforming numerical values to their verbal representations in Polish.
+ * It extends the {@link TextTransformerDecorator} class and overrides the transform and decorate methods.
+ * 
+ * <p>This class uses regular expressions to identify numerical values in the input text and converts them to their corresponding verbal forms.
+ * It supports integer values from 0 to 1000 and decimal values with up to two decimal places.</p>
+ * 
+ * <p>Example transformations:</p>
+ * <ul>
+ *   <li>"123" -> "sto dwadzieścia trzy"</li>
+ *   <li>"45.67" -> "czterdzieści pięć i sześćdziesiąt siedem setnych"</li>
+ *   <li>"0.1" -> "dziesięć setnych"</li>
+ *   <li>"0.06" -> "sześć setnych"</li>
+ * </ul>
+ * 
+ * <p>Logging is used to debug the transformation process, showing the original and transformed strings.</p>
+ * 
+ * <p>Note: This class assumes that numerical values in input text are formatted accordingly.</p>
+ * 
+ * @author  Krzysztof Smal
+ * @since   1.1
+ */
 public class TransformVerbalise extends TextTransformerDecorator{
+
+    /**
+     * Logger instance for logging events and messages within the TransformVerbalise class.
+     */
     private static final Logger logger = LoggerFactory.getLogger(TransformVerbalise.class);
+
+    /**
+     * Constructs a TransformVerbalise object with the specified TextTransformerInterface.
+     *
+     * @param transformer the TextTransformerInterface to be used for text transformation
+     */
     public TransformVerbalise(TextTransformerInterface transformer) {
         super(transformer);
     }
 
+    /**
+     * Transforms the given text by applying a series of transformations and decorations.
+     *
+     * @param text the input text to be transformed
+     * @return the transformed and decorated text
+     */
     public String transform(String text){
         String transformed = super.transform(text);
         String decorated = decorate(transformed);
         logger.debug(String.format("Transformed string %s to %s", text, decorated));
         return decorated;
     }
+    
+    
+    /**
+     * Transforms numerical values in the input text to their verbal representation in Polish.
+     *
+     * @param text the input text containing numerical values to be transformed
+     * @return the transformed text with numerical values verbalized in Polish
+     */
     @Override
     public String decorate(String text) {
         String number_regex = "\\b(?:0(?:\\.\\d+)?|[1-9]\\d*(?:\\.\\d+)?)\\b"; // "\\b\\d+(?:\\.\\d+)?\\b"
