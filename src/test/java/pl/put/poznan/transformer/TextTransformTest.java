33d64efd152d7fc63eb9de5e04851172ddb149f1
fmichalski03 <filip.michalski.1@student.put.poznan.pl>
2024-12-18 14:26:56 +0100

    tests_init

---
+++ src/test/java/pl/put/poznan/transformer/TextTransformTest.java
@@ -0,0 +1,45 @@
+package pl.put.poznan.transformer;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.ResponseEntity;
+
+import pl.put.poznan.transformer.logic.*;
+public class TextTransformTest {
+
+    private TextTransformerInterface transformer;
+
+    @BeforeEach
+    public void setUp(){
+        transformer = new TextTransformer();
+    }
+
+    @Test
+    @DisplayName("Pojedyncze powtorzenie slowa")
+    public void singleRepetitionTest(){
+        try{
+            transformer = (TextTransformerDecorator) TransformRemove.class.getDeclaredConstructor(TextTransformerInterface.class)
+            .newInstance(transformer);
+        } catch (Exception e) {
+                System.exit(-1);
+            }
+            Assertions.assertEquals("do na tu", transformer.transform("do do na na tu"));
+    }
+
+    @Test
+    @DisplayName("Kilkukritne powtorzenie slow")
+    public void consecutiveRepetition(){
+        try{
+            transformer = (TextTransformerDecorator) TransformRemove.class.getDeclaredConstructor(TextTransformerInterface.class)
+            .newInstance(transformer);
+        } catch (Exception e) {
+                System.exit(-1);
+            }
+            Assertions.assertEquals("do na tu", transformer.transform("do do do do na na na tu"));
+    }
+}
