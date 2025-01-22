b0482abf474a04f613ad0b7cb9ec058048f5fb40
PMazi <pawmaz02@gmail.com>
2025-01-22 14:38:37 +0100

    Dodano dokumentacje do klas realizujacych funkcje upper i capitalize

--- src/main/java/pl/put/poznan/transformer/logic/TransformCapitalize.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformCapitalize.java
@@ -3,12 +3,42 @@ package pl.put.poznan.transformer.logic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A class that decorates text by capitalizing the first letter of each word.
+ * This class extends {@link TextTransformerDecorator} and modifies the text so that every word starts with an uppercase letter.
+ *
+ * Example:
+ * <ul>
+ *   <li>Input: "this is a test"</li>
+ *   <li>Output: "This Is A Test"</li>
+ * </ul>
+ *
+ * @author      Paweł Mazurkiewicz
+ * @since       2.0
+ */
 public class TransformCapitalize extends TextTransformerDecorator{
+
+    /**
+     * Logger instance for logging events and messages within the {@code TransformCapitalize} class.
+     */
     private static final Logger logger = LoggerFactory.getLogger(TransformCapitalize.class);
+
+    /**
+     * Constructs a {@code TransformCapitalize} object with a reference to another {@link TextTransformerInterface}.
+     *
+     * @param transformer the base transformer to be decorated
+     */
     public TransformCapitalize(TextTransformerInterface transformer) {
         super(transformer);
     }
 
+    /**
+     * Transforms the given text by capitalizing each word, decorating
+     * the output of the base transformer, and logging the primary and decorated text.
+     *
+     * @param text the input text to transform
+     * @return the transformed text with each word capitalized
+     */
     public String transform(String text){
         String transformed = super.transform(text);
         String decorated = decorate(transformed);
@@ -16,6 +46,13 @@ public class TransformCapitalize extends TextTransformerDecorator{
         return decorated;
     }
 
+    /**
+     * Capitalizes the first letter of each word in the given text.
+     * Words are defined as sequences of characters separated by whitespace.
+     *
+     * @param text the input text to process
+     * @return the text with each word capitalized
+     */
     private String capitalize(String text) {
         if (text == null || text.isEmpty()) {
             return text;
@@ -28,6 +65,12 @@ public class TransformCapitalize extends TextTransformerDecorator{
         return result;
     }
 
+    /**
+     * Applies the capitalize transformation to the given text.
+     *
+     * @param text the input text to process
+     * @return the text with each word capitalized
+     */
     @Override
     public String decorate(String text) {
         return capitalize(text);
--- src/main/java/pl/put/poznan/transformer/logic/TransformUpper.java
+++ src/main/java/pl/put/poznan/transformer/logic/TransformUpper.java
@@ -3,18 +3,55 @@ package pl.put.poznan.transformer.logic;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * A class that decorates text by converting it to uppercase.
+ * This class extends {@link TextTransformerDecorator} and converts all letters in the input text to uppercase.
+ *
+ * Example:
+ * <ul>
+ *   <li>Input: "this is a test"</li>
+ *   <li>Output: "THIS IS A TEST"</li>
+ * </ul>
+ *
+ * @author      Paweł Mazurkiewicz
+ * @since       2.0
+ */
 public class TransformUpper extends TextTransformerDecorator{
+
+    /**
+     * Logger instance for logging events and messages within the {@code TransformUpper} class.
+     */
     private static final Logger logger = LoggerFactory.getLogger(TransformUpper.class);
+
+    /**
+     * Constructs a {@code TransformUpper} object with a reference to another {@link TextTransformerInterface}.
+     *
+     * @param transformer the base transformer to be decorated
+     */
     public TransformUpper(TextTransformerInterface transformer) {
         super(transformer);
     }
 
+    /**
+     * Transforms the given text by converting it to uppercase, decorating
+     * the output of the base transformer, and logging the primary and decorated text.
+     *
+     * @param text the input text to transform
+     * @return the transformed text in uppercase
+     */
     public String transform(String text){
         String transformed = super.transform(text);
         String decorated = decorate(transformed);
         logger.debug(String.format("Transformed string %s to %s", text, decorated));
         return decorated;
     }
+
+    /**
+     * Converts the given text to uppercase.
+     *
+     * @param text the input text to process
+     * @return the text converted to uppercase
+     */
     @Override
     public String decorate(String text) {
         return text.toUpperCase();
